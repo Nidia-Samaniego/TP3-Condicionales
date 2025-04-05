b5f4ec29cc52108b0ef2c93e6b1730305fe65f6b
nydsama <nidia.samaniego1@gmail.com>
2025-04-04 21:43:22 -0300

    se agrega tp-3 al seguimiento

---
+++ TP-3/condicional.py
@@ -0,0 +1,46 @@
+#actividad 1
+#edad= int(input("Ingrese su edad:"))
+
+#if edad >= 18:
+    #print("Sos mayor de edad")
+#else:
+    #print("Sos menor de edad")
+
+#actividad 2
+
+#nota= int(input("Ingrese su nota: "))
+
+#if nota >= 6:
+    #print("Aprobado")
+#else:
+    #print("desaprobado")
+
+#ejercicio 3
+
+#numero = int(input("Ingrese un numero: "))
+
+#if numero % 2 == 0 :
+    #print("Ha ingresado un numero par")
+#else:
+    #print("Por favor ingrese un numero par")
+
+#ejercicio 4
+
+#suedad= int(input("Ingrese su edad: "))
+
+#if suedad < 12:
+    #print("Usted es un Niño/a")
+#elif suedad >=12 and < 18:
+    #print("Usted es adolescente ")
+#elif suedad >= 18 and < 30:
+    #print("Usted es adulto/a joven")
+#else:
+    #print("Usted es mayo")
+
+
+#ejercicio 5 
+
+
+    
+   
+   
