88efa29cdf49bab189d8b2e548dd379f9c27a9c2
nydsama <nidia.samaniego1@gmail.com>
2025-04-04 23:40:48 -0300

    ejercios 5 y 6 completados

--- TP-3/condicional.py
+++ TP-3/condicional.py
@@ -30,15 +30,39 @@
 
 #if suedad < 12:
     #print("Usted es un Niño/a")
-#elif suedad >=12 and < 18:
+#elif suedad >=12 and suedad < 18:
     #print("Usted es adolescente ")
-#elif suedad >= 18 and < 30:
+#elif suedad >= 18 and suedad < 30:
     #print("Usted es adulto/a joven")
 #else:
     #print("Usted es mayo")
 
 
 #ejercicio 5 
+
+#contraseña= input("Ingrese una contraseña de entre 8 y 14 caracteres: ")
+#longitud = len(contraseña)
+
+#if longitud >=8 and longitud<= 14:
+    #print("Ha ingresado una contraseña correcta")
+#else:
+    #print("Por favor ingrese una contraseña de entre 8 y 14 caracteres")
+
+#ejercicio 6
+
+import random
+numeros_aletorios = [random.randint(1,100) for i in range(50)]
+from statistics import mode, median, mean
+mean (numeros_aletorios)
+
+if numeros_aletorios == mode:
+    print("Hay sesgo positivo")
+elif numeros_aletorios == median:
+    print("Hay sesgo negativo")
+else:
+    print("sin sesgo")
+
+
 
 
     
