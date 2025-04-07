4ea4be5e53e2a1769556aac2ad5f480e182be126
nydsama <nidia.samaniego1@gmail.com>
2025-04-06 23:23:20 -0300

    Ejercicios finalizados

--- TP-3/condicional.py
+++ TP-3/condicional.py
@@ -81,18 +81,68 @@
 #print("1- En mayuscula")
 #print("2- En minuscula")
 #print("3- La primer letra en mayuscula")
-#opcion =input( "Ingresa: 1, 2, o 3: " )
+#opcion =int(input( "Ingresa: 1, 2, o 3: " ))
 
-#if opcion == "1":
+#if opcion == 1:
     #print(nombre.upper())
-#elif opcion == "2":
+#elif opcion == 2:
     #print(nombre.lower())
-#elif opcion == "3":
+#elif opcion == 3:
     #print(nombre.title())
 #else:
     #print("Por favor ingrese una opcion valida")
 
 #ejercicio 9
+
+#terremoto= int(input("ingrese magnitud del terremoto: "))
+
+#if terremoto < 3:
+    #print("Muy leve (Imperceptible)")
+#elif terremoto >= 3 and terremoto < 4:
+    #print ("Leve (Ligeramente perceptible)")
+#elif terremoto >= 4 and terremoto < 5:
+    #print ("Moderado (Sentido por personas, pero generalmente no causa daño)")
+#elif terremoto >=5 and terremoto < 6:
+    #print ("Fuerte (Puede causa daño en estructuras debiles)")
+#elif terremoto >=6 and terremoto < 7:
+    #print("Muy Fuerte (Puede causar daños significativos)")
+#else:
+    #print("Extremo (Puede causar graves daños a gran escala)")
+
+#ejercicio 10
+
+hemisferio = input("¿En qué hemisferio te encuentras? (N/S): ").strip().upper()
+mes = int(input("Ingresa el número del mes (1-12): "))
+dia = int(input("Ingresa el día del mes: "))
+
+if hemisferio == 'N':
+    if (mes == 12 and dia >= 21) or (mes <= 2) or (mes == 3 and dia <= 20):
+         print("estacion: Invierno")
+    elif (mes == 3 and dia >= 21) or (mes <= 5) or (mes == 6 and dia <= 20):
+        print("estacion: Primavera")
+    elif (mes == 6 and dia >= 21) or (mes <= 8) or (mes == 9 and dia <= 20):
+        print("estacion: Verano")
+    elif (mes == 9 and dia >= 21) or (mes <= 11) or (mes == 12 and dia <= 20):
+        print("estacion: Otoño")
+    else:
+        print("estacion: fecha invalida")
+
+elif hemisferio == 'S':
+    if (mes == 12 and dia >= 21) or (mes <= 2) or (mes == 3 and dia <= 20):
+        print("estacion: Verano")
+    elif (mes == 3 and dia >= 21) or (mes <= 5) or (mes == 6 and dia <= 20):
+        print("estacion: Otoño")
+    elif (mes == 6 and dia >= 21) or (mes <= 8) or (mes == 9 and dia <= 20):
+        print("estacion: Invierno")
+    elif (mes == 9 and dia >= 21) or (mes <= 11) or (mes == 12 and dia <= 20):
+        print("estacion: Primavera")
+    else:
+        print("estacion: fecha invalida")
+else:
+    print("estacion: Hemisferio no válido. Por favor, ingresa 'N' o 'S'.")
+    
+
+
 
 
 
